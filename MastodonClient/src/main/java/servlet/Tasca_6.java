e89c001b0b2dad677e9e3fd799ca1588c65650a9
carlos.fabian.cieza <carlos.fabian.cieza@estudiantat.upc.edu>
2026-02-27 11:18:20 +0100

    Tasca #6 Obtenir followers de fib_asw

--- MastodonClient/src/main/java/servlet/Tasca_6.java
+++ MastodonClient/src/main/java/servlet/Tasca_6.java
@@ -4,14 +4,37 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.hc.client5.http.fluent.Request;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ResourceBundle;
 
 @WebServlet(value = "/")
 public class Tasca_6 extends HttpServlet {
 
+    private static final String BASE_URI = "https://mastodont.cat/api/v1";
+    private static final String TOKEN = ResourceBundle.getBundle("token").getString("token");
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            // ID del compte fib_asw (hardcodejat)
+            String accountId = "109862447110628983";
+
+            // Obtenir el darrer tut (status) del compte fib_asw
+            String statusUri = BASE_URI + "/accounts/" + accountId + "/followers";
+            String output = Request.get(statusUri)
+                    .addHeader("Authorization", "Bearer " + TOKEN)
+                    .execute()
+                    .returnContent()
+                    .asString();
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
         response.setContentType ("text/html");
         response.setCharacterEncoding("UTF-8");
